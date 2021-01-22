a43ce60a0ce4f673a16cf5d3b0e5558629800167
Ismael Bonilla <ibonilla@BAOPARTN.COM>
2021-01-22 21:15:07 +0100

    avanzamos

--- movements/forms.py
+++ movements/forms.py
@@ -2,19 +2,67 @@
 from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, FloatField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+import sqlite3
+from movements import app
+
+DBFILE = app.config['DBFILE']
+
+def ask(query, params=()):
+    conn = sqlite3.connect(DBFILE)
+    c = conn.cursor()
+
+    c.execute(query, params)
+    conn.commit()
+
+    filas = c.fetchall()
+    conn.close()
+
+
+    if len(filas) == 0:
+        return filas
+
+    columnNames = []
+    for columnName in c.description:
+        columnNames.append(columnName[0])
+
+    listaDeDiccionarios = []
+
+    for fila in filas:
+        d = {}
+        for ix, columnName in enumerate(columnNames):
+            d[columnName] = fila[ix]
+        listaDeDiccionarios.append(d)
+
+    return listaDeDiccionarios
 
 
 
 class MovementForm(FlaskForm):
+    disponibles = ask('SELECT to__currency FROM movements;')
 
-    date = DateField('Fecha', validators=[DataRequired()])
-    time = DateField('Hora', validators=[DataRequired()])
+    listadisponibles=[]
+
+    listadisponibles.append('EUR')
+
+    for elemento in disponibles:
+        if elemento['to__currency'] not in listadisponibles:
+            listadisponibles.append(elemento['to__currency'])
+
+    print (listadisponibles)
+
+    date = StringField('Fecha')
+    time = StringField('Hora')
+
+
+
 
     #from_currency = StringField('From_Currency', validators=[DataRequired()])
-    from_currency = SelectField('Moneda origen', choices=('EUR', 'ETH', 'LTC', 'BNB', 'EOS', 'XLM', 'TRX', 'BTC', 'XRP', 'BCH', 'USDT', 'BSV', 'ADA'), validators=[DataRequired()])
+    from_currency = SelectField('Moneda origen', choices=listadisponibles, validators=[DataRequired()])
     from_quantity = FloatField('Cantidad de moneda de origen', validators=[DataRequired()])
+    
 
-    to_currency = SelectField('Moneda origen', choices=('EUR', 'ETH', 'LTC', 'BNB', 'EOS', 'XLM', 'TRX', 'BTC', 'XRP', 'BCH', 'USDT', 'BSV', 'ADA'), validators=[DataRequired()])
+    to__currency = SelectField('Moneda destino', choices=('EUR', 'ETH', 'LTC', 'BNB', 'EOS', 'XLM', 'TRX', 'BTC', 'XRP', 'BCH', 'USDT', 'BSV', 'ADA'), validators=[DataRequired()])
+    submit = SubmitField('Calcular')
     to_quantity = FloatField('Cantidad de moneda de destino', validators=[DataRequired()])
 
-    submit = SubmitField('Aceptar')
+    submit = SubmitField('Comprar')
--- movements/views.py
+++ movements/views.py
@@ -3,7 +3,7 @@ from movements.forms import MovementForm
 from flask import render_template, request, url_for, redirect
 import csv
 import sqlite3
-from datetime import date
+from datetime import date, datetime
 
 DBFILE = app.config['DBFILE']
 monedas = ('EUR', 'ETH', 'LTC', 'BNB', 'EOS', 'XLM', 'TRX', 'BTC', 'XRP', 'BCH', 'USDT', 'BSV', 'ADA')
@@ -44,21 +44,35 @@ def listaIngresos():
     return render_template("movementsList.html", datos=ingresos)
 
 @app.route('/creaalta', methods=['GET', 'POST'])
+
+
 def nuevoIngreso():
+    
+
+
 
     form = MovementForm()
     
     if request.method == 'POST':
+        now = datetime.now()
+        today =date.today()
+        fecha = today
+        hora = now.time()
+        form.date.data = str(fecha)
+        form.time.data = str(hora)
+
+        
 
         if form.validate():
-            consulta('INSERT INTO movements (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?, ?, ? ,? ,?, ? );', 
+
+            consulta('INSERT INTO movements (date, time, from_currency, from_quantity, to__currency, to_quantity) VALUES (?, ?, ? , ?, ?, ?);', 
                         (   
                             form.date.data,
                             form.time.data,
                             form.from_currency.data,
                             form.from_quantity.data,
-                            form.to_currency.data,
-                            fotm.to_quantity.data
+                            form.to__currency.data,
+                            form.to_quantity.data
                         )
                     )
             return redirect(url_for('listaIngresos'))
@@ -66,50 +80,3 @@ def nuevoIngreso():
         else:
             return render_template("alta.html", form=form)
     return render_template("alta.html", form=form)
-
-
-@app.route("/modifica/<id>", methods=['GET', 'POST'])
-def modificaIngreso(id):
-    
-    if request.method == 'GET':
-
-        registro = consulta('SELECT fecha, concepto, cantidad, id FROM movimientos where id = ?', (id,))[0] 
-        registro['fecha'] = date.fromisoformat(registro['fecha'])
-        form = MovementForm(data=registro)
-
-        return render_template("modifica.html", form=form, id=id)
-
-    else:
-        form = MovementForm()
-        if form.validate():
-            consulta('UPDATE movimientos SET fecha = ?, concepto= ?, cantidad = ? WHERE id = ?',
-                    (
-                    request.form.get('fecha'),
-                    request.form.get('concepto'),
-                    float(request.form.get('cantidad')),
-                    id
-                    )
-            )
-
-            return redirect(url_for("listaIngresos"))
-        else:
-            return render_template("modifica.html", form=form, id=id)
-
-@app.route("/elimina/<id>", methods=['GET', 'POST'])
-def eliminaIngreso(id):
-    
-    if request.method == 'GET':
-
-        registro = consulta('SELECT fecha, concepto, cantidad, id FROM movimientos where id = ?', (id,))[0] 
-        registro['fecha'] = date.fromisoformat(registro['fecha'])
-        form = MovementForm(data=registro)
-
-        return render_template("elimina.html", form=form, id=id)
-
-    else:
-        form = MovementForm()
-        if form.validate():
-            consulta('DELETE FROM movimientos WHERE id = ?', (id,))
-            return redirect(url_for("listaIngresos"))
-        else:
-            return render_template("elimina.html", form=form, id=id)
